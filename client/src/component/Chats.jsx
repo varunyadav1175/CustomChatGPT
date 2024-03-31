59d66e47f81c3469507d0e53947454869af0656b
nikhilsingh132 <89939050+nikhilsingh132@users.noreply.github.com>
2024-03-31 18:37:38 +0530

    api integrated

--- client/src/component/Chats.jsx
+++ client/src/component/Chats.jsx
@@ -3,6 +3,7 @@ import React, { useState, useContext } from "react";
 import ChatMessageGpt from "./ChatMessageGpt";
 import ChatMessageUser from "./ChatMessageUser";
 import { chatdetail } from "@/app/home/page";
+import {ColorTheme} from "./ColorTheme";
 import axios from "axios";
 
 
@@ -11,27 +12,28 @@ const Chats = () => {
   const [input, setInput] = useState("");
 
   const handleSubmit = async (e) => {
-    // e.preventDefault();
-    // let newMessage = { user: "me", message: input };
-    // let chatLogNew=[...chatlog,newMessage];
-    // setChatlog(chatLogNew);
-    // setInput("");
-    // const chatmessage = newMessage.message;
-    // try {
-    //   const response = await axios.post("http://localhost:8000/getAnswer", {
-    //     chatmessage,
-    //   });
-    //   console.log("Message sent successfully:", response.data.data);
-    //   const gptMessage = { user: "gpt", message: response.data.data };
-    //   setChatlog([...chatLogNew, gptMessage]);
-    // } catch (error) {
-    //   console.error("Error sending message:", error);
-    // }
+    e.preventDefault();
+    let newMessage = { user: "me", message: input };
+    let chatLogNew=[...chatlog,newMessage];
+    setChatlog(chatLogNew);
+
+    setInput("");
+    const chatmessage = newMessage.message;
+    console.log("chatmessage", chatmessage);
+    try {
+      const response = await axios.get(`http://localhost:5000/api/chat?query=${chatmessage}`);
+      console.log("Message sent successfully:", response);
+      const gptMessage = { user: "gpt", message: response.data.response };
+      setChatlog([...chatLogNew, gptMessage]);
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
   };
 
   return (
     <>
-      <div className="bg-[#212121] w-full h-[100vh] flex flex-col overflow-y-scroll">
+      <div className="dark:bg-[#212121] bg-white w-full h-[100vh] flex flex-col overflow-y-scroll">
+      <ColorTheme/>
         <div className="chat-log text-left">
           {chatlog.map(({ user, message, index }) => {
             if (user === "me") {
@@ -49,7 +51,7 @@ const Chats = () => {
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
                 rows="1"
-                className="bg-[#212121] border border-gray-500 w-[50rem] h-[3rem] rounded-lg outline-none text-white p-[10px]"
+                className="dark:bg-[#212121] bg-white border border-black dark:border-gray-500 w-[50rem] h-[3rem] rounded-lg outline-none text-black dark:text-white p-[10px]"
                 placeholder="Message ChatGPT"
               ></input>
             </form>
